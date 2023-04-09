ce60f7f92f8edfa9a23002cf59869e65b8160df9
TakibHossen <mdtakibhossen1@gmail.com>
2023-04-09 21:43:12 +0600

    delete btn implement

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Tshirt from '../Tshirt/Tshirt';
 import Cart from '../Cart/Cart';
 import './Home.css'
+import toast from 'react-hot-toast';
 
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tshirt =>{
+        const exists = cart.find(ts => ts._id === tshirt._id);
+        if(exists){
+            toast('You have already added this T-Shirt')
+        }
+        else{
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+        }
+        
+    }
+
+    const handleRemoveFromCart = id =>{
+        const remaining = cart.filter(ts => ts._id !== id);
+        setCart(remaining);
+    }
 
     return (
         <div className='home-container'>
@@ -14,11 +33,15 @@ const Home = () => {
                     tshirts.map(tshirt => <Tshirt
                         key={tshirt._id}
                         tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
                     ></Tshirt>)
                 }
             </div>
             <div className='cart-container'>
-                <Cart></Cart>
+                <Cart 
+                    cart={cart}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                ></Cart>
             </div>
         </div>
     );
